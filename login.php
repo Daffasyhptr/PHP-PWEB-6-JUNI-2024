13e732b4bc91dada965b8d34c40a7356684ecfed
Daffa555 <155145439+Daffasyhptr@users.noreply.github.com>
2024-06-13 16:45:41 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,21 @@
+
+<?php
+    $NIM           = $_POST[ 'NIM'];
+    $Password      = $_POST[ 'Password'];
+
+    $N = 'A12.2020.06484'; //NIM
+    $P = '12345';//Password
+
+    if($NIM == $N AND $Password == $P)
+    {
+        session_start();
+        $_SESSION['nama'] = $NIM;
+        $_SESSION['is_logged_id'] = TRUE;
+
+        header("Location: halaman1.php");
+    
+    }
+    else
+    {
+        echo "NIM atau Password salah";
+    }
